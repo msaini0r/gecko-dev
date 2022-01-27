600afccd50fd229c2cebfe479551417f4cedd898
Brian Hackett <bhackett1024@gmail.com>
2022-01-27 11:08:54 -0700

    Support running build/test branch action against release branch when … (#691)
    * Support running build/test branch action against release branch when a driver revision is specified
    
    * Add support for branch slot input

--- .github/actions/build-test-branch/main.js
+++ .github/actions/build-test-branch/main.js
@@ -8,25 +8,37 @@ const {
 const branchName = getBranchName(process.env.GITHUB_REF);
 console.log("BranchName", branchName);
 
-if (branchName.includes("webreplay-release")) {
-  console.error("Use build/test action for release branch");
-  process.exit(1);
-}
-
 const replayRevision = getLatestReplayRevision();
 
 const driverRevision = process.env.INPUT_DRIVER_REVISION;
 console.log("DriverRevision", driverRevision);
 
+if (branchName.includes("webreplay-release") && !driverRevision) {
+  console.error("Use build/test action for release branch");
+  process.exit(1);
+}
+
 const clobberInput = process.env.INPUT_CLOBBER;
 console.log("Clobber", clobberInput);
 const clobber = clobberInput == "true";
 
+const slotInput = process.env.INPUT_SLOT;
+console.log("Slot", slotInput);
+const slot = slotInput ? +slotInput : undefined;
+
 const buildOnly = !!process.env.BUILD_ONLY;
 console.log("BuildOnly", buildOnly);
 
+let requestName = `Gecko Build/Test Branch ${branchName} ${replayRevision}`;
+if (driverRevision) {
+  requestName += ` driver ${driverRevision}`;
+}
+if (slot) {
+  requestName += ` slot ${slot}`;
+}
+
 sendBuildTestRequest({
-  name: `Gecko Build/Test Branch ${branchName} ${replayRevision}${driverRevision ? " driver " + driverRevision : ""}`,
+  name: requestName,
   tasks: [
     ...platformTasks("macOS"),
     ...platformTasks("linux"),
@@ -42,6 +54,7 @@ function platformTasks(platform) {
       runtime: "gecko",
       revision: replayRevision,
       branch: branchName,
+      branchSlot: slot,
       driverRevision,
       clobber,
     },
